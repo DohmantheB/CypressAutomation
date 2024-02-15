c9fa884f7f04d35ef8dfbdf4ef1b9c89324aff5b
DohmantheB <dohmanburak@gmail.com>
2024-02-15 14:43:02 +0300

    Add:new test cases

--- cypress/e2e/testStructure.cy.js
+++ cypress/e2e/testStructure.cy.js
@@ -19,8 +19,22 @@ describe('Context: My First Tests', () => {
 
     it('Opening a web application',()=>{
         cy.visit('https://practice.cydeo.com/');
-        cy.get(':nth-child(9) > a').click();
-        cy.get('.nav-link').click();
+    })
+    
+    it('Test 2', ()=> {
+        expect(false).to.equal(false);
+    })
+
+    it('Test 3', () =>{
+        expect(false).not.to.equal(true);
+    })
+
+    it('Test 4', () =>{
+        expect(5).to.equal(5);
+    })
+
+    it('Test 5', () =>{
+        expect(true).to.equal('5'==5);
     })
 
 
