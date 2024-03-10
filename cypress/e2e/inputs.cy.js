f896c02831f53a758b40b36681a4d92455700477
DohmantheB <dohmanburak@gmail.com>
2024-03-10 23:14:41 +0300

    add: fictures/departments.json

--- cypress/e2e/inputs.cy.js
+++ cypress/e2e/inputs.cy.js
@@ -26,11 +26,11 @@ describe('Input Forms Tests', () => {
 
   it.skip('Check different radio button actions', () => {
     cy.get('input[type="radio"]')
-      //cy.get('.radio') //locator olarak bu da kullanılabilir.  -->  input[type="radio"]
+      // cy.get('.radio') //locator olarak bu da kullanılabilir.  -->  input[type="radio"]
       //  .find('[type=radio]')
       .then((radio) => {
-        //get all radio buttons, select the 1st one and verify that it is checked
-        cy.wrap(radio).first().check().should('be.checked'); //cypress works in a chainable functions structure
+        // get all radio buttons, select the 1st one and verify that it is checked
+        cy.wrap(radio).first().check().should('be.checked'); // cypress works in a chainable functions structure
         /**
          * radio: is jQuery element, cy.wrap(radio): turns it into cy obj
          * first() : select first element
@@ -38,24 +38,33 @@ describe('Input Forms Tests', () => {
          * should() : verifies whatever I provide as param  'be.checked'
          */
 
-        //get all radio buttons, select the 2nd one and verify that it is checked
+        // get all radio buttons, select the 2nd one and verify that it is checked
 
         cy.wrap(radio).eq(1).check().should('be.checked');
         cy.get('[data-bv-icon-for="gender"]').should('be.visible');
-        //checked 3rd radio button is UNCHECKED
+        // checked 3rd radio button is UNCHECKED
         cy.wrap(radio).eq(2).should('not.be.checked');
       });
   });
 
-  it('Check different checkbox actions', () => {
+  it.skip('Check different checkbox actions', () => {
     // get all check boxes, select JAVA is checked
     cy.get('[type="checkbox"]').then((checkBoxes) => {
       cy.wrap(checkBoxes).eq(1).check().should('be.checked');
-      //unchecked java from the page
-      //verify uncheck
+      // unchecked java from the page
+      // verify uncheck
       cy.wrap(checkBoxes).eq(1).uncheck().should('not.be.checked');
-      //verify 3rd one as a value Javascript and check and verify
+      // verify 3rd one as a value Javascript and check and verify
       cy.wrap(checkBoxes).eq(2).should('have.value', 'javascript').check().should('be.checked');
     });
+  });
+  it.skip('Check selection of a single choice from a select dropdown', () => {
+    cy.get('select[name="job_title"]').select('SDET');
+    // assert that dropdown has correct text after selecting
+    cy.get('select[name="job_title"]').contains('SDET');
+  });
+
+  it('Check selection of all select dropdowns options', () => {
+    // we will provide out test data through fixtures folder as JSON obj, then use that data to verify select values.
   });
 });
